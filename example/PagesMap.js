aa6cc9d69433959a28c0b9e9eec8184bbd6ae170
Stefan Zerkalica <zerkalica@gmail.com>
2015-12-15 13:16:22 +0300

    config example

---
+++ example/PagesMap.js
@@ -0,0 +1,9 @@
+import TodoViewPage from 'todo/components/pages/TodoViewPage'
+import TodoAppIndexPage from 'todo/components/pages/TodoAppIndexPage'
+
+const PagesMap = {
+    TodoAppIndexPage,
+    TodoViewPage
+}
+
+export default PagesMap
